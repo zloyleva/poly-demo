4c7e7c9c17f617dca24cf85a2c91b4f6c5cab681
zloyleva <zloyleva@gmail.com>
2016-11-01 21:48:39 +0200

    Add polymorphic association to models

--- db/migrate/20161101175420_create_comments.rb
+++ db/migrate/20161101175420_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.text :contents
-
+      t.text :content
+      t.references :commentable, polymorphic: true
       t.timestamps
     end
   end
